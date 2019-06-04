0a60146fdd21ff385f805549e668dfd50634e516
Blake Stoner <stonerblake@gmail.com>
2019-06-04 17:00:16 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,24 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  character_hash = response_hash["results"].find do |char|
+    char["name"].downcase == character_name
+  end
+
+  if character_hash == nil
+    puts "The force is not strong with you. Try again."
+    return []
+  end
+
+  character_films = character_hash["films"]
+  films_info = []
+  character_films.each {|movie_link|
+    movie_string = RestClient.get(movie_link)
+    movie_info = JSON.parse(movie_string)
+    films_info.push(movie_info)
+  }
+  return films_info
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -18,15 +36,18 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
+
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each_with_index {|film, index|
+    puts "#{index + 1}. #{film["title"]}"
+  }
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
